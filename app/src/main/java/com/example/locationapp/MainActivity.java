85cb2dee216064bf3d88d32ddfbd1ca973fe0aba
Thilina Fernando <mtfernando@deakin.edu.au>
2021-05-12 17:12:01 +1000

    Places API functionality added

--- app/src/main/java/com/example/locationapp/MainActivity.java
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.locationapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView locationLatLong;
+    TextView locationLatLong, searchResult;
 
     LocationManager locationManager;
     LocationListener locationListener;
+    Button placeSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         locationLatLong = findViewById(R.id.locationLatLong);
+        placeSearch = findViewById(R.id.placeSearch);
+        searchResult =findViewById(R.id.searchText);
 
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener(){
@@ -40,7 +56,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        Places.initialize(getApplicationContext(), "AIzaSyAuuYM85VQZh7Fs1Yw6mcwd2CmjFH8VVf4");
+
+        placeSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fieldList = new ArrayList<>();
+                fieldList.add(Place.Field.NAME);
+                fieldList.add(Place.Field.ADDRESS);
+                fieldList.add(Place.Field.LAT_LNG);
+
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(MainActivity.this);
+                startActivityForResult(intent, 100);
+            }
+        });
+
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -48,6 +82,17 @@ public class MainActivity extends AppCompatActivity {
 
         if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==100 && resultCode == RESULT_OK){
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            String text = place.getName()+"" + place.getAddress();
+            searchResult.setText(text);
         }
     }
 }
